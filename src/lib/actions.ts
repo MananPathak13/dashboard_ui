954e5f22988ea91fc30d75d43af714102852653d
Manan Pathak <pathakmanan5@gmail.com>
2024-12-28 21:33:15 -0500

    Updated version of this code to move to laptop

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,4 @@
 "use server";
-import { revalidatePath } from "next/cache";
 import { ClassSchema, SubjectSchema, TeacherSchema } from "./formValidationSchemas";
 import prisma from "./prisma";
 import { clerkClient } from "@clerk/clerk-sdk-node";
